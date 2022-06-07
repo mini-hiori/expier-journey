0c1eeb8a85b0346c87f92c3105c5a9985e879a1e
mini-hiori <phantombisco@gmail.com>
2022-06-08 04:23:35 +0900

    ビルドdone

--- docs/_next/static/aAP9U06ZrHbR01V5m8QU9/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/a32bfb27ed42e441.css","static/chunks/pages/index-1243812fd3ed275f.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
